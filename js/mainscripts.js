c13b64af2e680e08e6b60e36d01b852ee10c5d17
Feifan Zhou <fz75@cornell.edu>
2013-03-12 01:05:15 -0400

    Fixed problem where user signup info wasn't getting stored in the database.
    Asana link:
    https://app.asana.com/0/3727565452987/4448088559214

--- js/mainscripts.js
+++ js/mainscripts.js
@@ -22,17 +22,4 @@ function newUserSignup() {
 			$("#signup").html(outputString);
 		}
 	});
-	/* $.ajax({
-		url: 'useremailsignup.php',
-		type: 'POST',
-		data: userData,
-		dataType: 'json'
-	}).done(function(reply) {
-		alert("AJAX return");
-		console.log("Reply: " + reply);
-		var fname = reply['fname'];
-		var outputString = "<p>" + fname + ", thanks for your interest! We hope you're as excited about nepTune as we are. If you have any questions, please send <a href='mailto:future@neptunemusic.co'>send us a message</a>!</p>";
-		console.log("OutputString: " + outputString);
-		$("#signup").html(outputString);
-	}); */
 }
--- useremailsignup.php
+++ useremailsignup.php
@@ -1,7 +1,10 @@
 <?php
-	$fname = mysql_real_escape_string($_POST["fname"]);
+	/* $fname = mysql_real_escape_string($_POST["fname"]);
 	$lname = mysql_real_escape_string($_POST["lname"]);
-	$email = mysql_real_escape_string($_POST["email"]);
+	$email = mysql_real_escape_string($_POST["email"]); */
+	$fname = $_POST["fname"];
+	$lname = $_POST["lname"];
+	$email = $_POST["email"];
 	$services_json = json_decode(getenv("VCAP_SERVICES"),true);
 	$mysql_config = $services_json["mysql-5.1"][0]["credentials"];
 	$username = $mysql_config["username"];
@@ -9,14 +12,21 @@
 	$hostname = $mysql_config["hostname"];
 	$port = $mysql_config["port"];
 	$dbname = $mysql_config["name"];
-	$link = mysqli_connect("$hostname", $username, $password);
-	$db_selected = mysql_select_db("leads", $link);
-	if (mysqli_connect_errno($link)) {
- 		print "Error: " . mysql_error();
+	$link = new mysqli("$hostname", $username, $password, $dbname);
+	if ($link->errno) {
+ 		print ("Error: " . $link->error);
  		die();
  	}
-	mysql_query("INSERT INTO leads (fname, lname, email) VALUES ('" . $fname . "', '" . $lname . "', '" . $email . "')");
-	mysqli_close($link);
+ 	$fname = $link->escape_string($fname);
+ 	$lname = $link->escape_string($lname);
+ 	$email = $link->escape_string($email);
+ 	$queryString = "INSERT INTO leads (fname, lname, email) VALUES ('" . $fname . "', '" . $lname . "', '" . $email . "')";
+	$insertResult = $link->query($queryString);
+	if (!$insertResult) {
+		print ("Error inserting");
+		die();
+	}
+	$link->close();
 	$fh = fopen('signups.txt', 'a+') or die("Error can't open text file");
 	$newEntry = $_POST["fname"] . "|^|^|" . $_POST["lname"] . "|^|^|" . $_POST["email"];
 	fwrite($fh, $newEntry);
